09e659c25d1cd829102fffed5e707e5c88792819
Marshalll3302 <marshalll3302@gmail.com>
2024-11-01 11:50:39 -0700

    test10

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,7 +11,7 @@ def test_hello_world(client):
     """Test the hello world route."""
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
+    assert response.data == b'{"message":"Hello, World!"}\n'
     
     
 def test_add(client):
